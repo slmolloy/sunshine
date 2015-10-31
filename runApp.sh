93b20176346e95c65e21ef9b77794daf44f2491e
Scott Molloy <scott.l.molloy@gmail.com>
2015-10-30 17:02:39 -0700

    Add sleep time between install and activity start
    Without sleep time the activity often wasn't ready to be started. There
    is a delay between app install completion and the activity being ready
    to be called.

--- runApp.sh
+++ runApp.sh
@@ -2,4 +2,5 @@
 
 gradle assembleDebug
 adb install -r app/build/outputs/apk/app-debug-unaligned.apk
+sleep 5
 adb shell am start -n com.example.android.sunshine.app/com.example.android.sunshine.app.MainActivity
